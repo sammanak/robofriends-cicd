35d39b34d2a8ce725c991e7558db524c5bff69ab
sammanak <naknakss@gmail.com>
2018-12-05 10:35:49 +0700

    Prettier Code

---
+++ src/components/CounterButton2.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class CounterButton2 extends Component {
+		constructor() {
+      super();
+      this.state = {
+        count: 0
+      }
+    }
+		shouldComponentUpdate(nextProps, nextState) {
+      if(this.state.count !== nextState.count){
+        return true;
+      }
+      return false;
+    }
+    
+    updateCount = () => {
+      this.setState( state => {
+        return { count: state.count + 1 }
+      })
+    }
+
+    render() {
+			   return (
+                  <button 
+              id='counter' 
+                color={this.props.color} 
+                      onClick={this.updateCount}>
+                              Count2: {this.state.count}
+              </button>
+      );
+    }    
+} 
+
+export default CounterButton2;
